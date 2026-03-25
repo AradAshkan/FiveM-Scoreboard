d54b60f2457ec4c016221c2920ba5b5c06ddcc4f
Aяad Ashkan <65897590+AradAshkan@users.noreply.github.com>
2026-03-26 01:00:18 +0330

    Update author info and maintain framework support
    Updated author information and retained framework support.

--- ScoreBoard/fxmanifest.lua
+++ ScoreBoard/fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version 'cerulean'
 game 'gta5'
 
-author 'Your Name'
+author 'Arad Ashkan | https://github.com/aradashkan'
 description 'Modern Scoreboard with Dark Red Theme'
 version '1.0.0'
 
@@ -21,14 +21,7 @@ files {
     'html/script.js'
 }
 
--- پشتیبانی از فریمورک‌ها
 shared_scripts {
     '@es_extended/imports.lua',
     '@qb-core/import.lua'
 }
-
--- یا اگر از فریمورک خاصی استفاده می‌کنی:
--- dependencies {
---     'es_extended',
---     'qb-core'
--- }
